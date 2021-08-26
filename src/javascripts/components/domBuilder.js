933136f4090759770576e7f382c09bf43eb73010
DerekMalone <laxmalone@gmail.com>
2021-08-25 21:16:28 -0500

    adding html elements to domBuilder

---
+++ src/javascripts/components/domBuilder.js
@@ -0,0 +1,9 @@
+cosnt domBuilder = () => {
+    document.querySelector('#app').innerHTML = `
+    <div id="navigation"></div>
+    <div id="main-container">
+    <div id="add-bear"></div>
+    <div id="best-bear"></div>
+    </div>
+    `;
+};
